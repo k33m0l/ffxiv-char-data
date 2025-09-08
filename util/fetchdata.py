e929aca8da07dfebdddc6a4ef10861804dbf64d2
k33m0l <k33m0l@users.noreply.github.com>
2025-09-08 20:21:45 +0200

    Data fetching iterated

---
+++ util/fetchdata.py
@@ -0,0 +1,39 @@
+import boto3
+import pandas
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource("dynamodb")
+TABLE_NAME = "FFXIV"
+LIMIT = 200
+
+table = dynamodb.Table(TABLE_NAME)
+
+def fetch_items():
+    global start_key
+    global items
+    if start_key is not None:
+        response = table.query(
+            IndexName='status-index',
+            KeyConditionExpression=Key("status").eq("DONE"),
+            ExclusiveStartKey=start_key,
+            Limit=LIMIT,
+        )
+    else:
+        response = table.query(
+            IndexName='status-index',
+            KeyConditionExpression=Key("status").eq("DONE"),
+            Limit=LIMIT,
+        )
+
+    items += response.get("Items", [])
+    start_key = response.get("LastEvaluatedKey")
+
+start_key = None
+items = []
+while True:
+    fetch_items()
+    if not start_key:
+        break
+
+df = pandas.DataFrame(items)
+df.to_csv("results.csv", index=False)
