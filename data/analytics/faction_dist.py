7c12ff43d9ad50cda622874542dc83a97508b349
k33m0l <k33m0l@users.noreply.github.com>
2025-11-05 21:32:13 +0100

    fix pie chart colous

--- data/analytics/faction_dist.py
+++ data/analytics/faction_dist.py
@@ -6,17 +6,17 @@ data = pandas.read_csv('../../resources/cleaned.csv')
 explode = (0.05,0.05,0.05)
 textprops = {
     'size': 16,
-    'color':'white',
+    'color':'black',
     'weight':'bold'
 }
 data['City-state'].value_counts().plot.pie(
-    autopct='%1.1f%%',
+    autopct='%1.2f%%',
     startangle=90,
     explode=explode,
     shadow=True,
     textprops=textprops,
-    frame=True,
 )
 
 pyplot.tight_layout()
 pyplot.show()
+
